161392f7b980042d1c28ebb7a257e29ae4777af4
unknown <M.Ben-jabir@uot.edu.ly>
2017-10-23 12:57:05 +0200

    from geeksforgeeks

--- BTree.java
+++ BTree.java
@@ -5,153 +5,156 @@
  */
 package project.pkg2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author Mahmoud
  */
+class BTreeNode {
+	int[] keys;
+	BTreeNode[] child;
+	int n;
+	boolean leaf;
+
+	// Constructor
+	BTreeNode(int t, boolean l) {
+		keys = new int[2 * t - 1];
+		child = new BTreeNode[2 * t];
+		leaf = l;
+		n = 0;
+	}
+
+	BTreeNode search(int k) {
+		int i = 0;
+		for (i = 0; i < n; i++) {
+			if (keys[i] >= k)
+				break;
+		}
+		if (i < n && keys[i] == k)
+			return this;
+		if (leaf == true)
+			return null;
+		//System.out.println(i);
+		return child[i].search(k);
+	}
+
+	void traverse(int l) {
+		for (int i = 0; i < n; i++) {
+			if (!leaf)
+				child[i].traverse(l + l);
+			System.out.print("Level : " + l + " : ");
+			System.out.print(keys[i] + " ");
+			System.out.println();
+		}
+		if (!leaf)
+			child[n].traverse(l + 1);
+	}
+
+	void insertNonFull(int k, int t) {
+		if (leaf) {
+			int x = n;
+			while (x > 0 && keys[x - 1] > k) {
+				keys[x] = keys[--x];
+			}
+			keys[x] = k;
+			n++;
+		} else {
+			int x = 0;
+			while (x < n && keys[x] < k)
+				x++;
+			if (child[x].n == (2 * t - 1))// Child is full
+			{
+				splitChild(x, this, t);
+				int i = x;
+				int m = this.child[x].n - 1;
+				// this.leaf=false;
+				if (this.child[x].keys[m] < k)
+					i++;
+				this.child[i].insertNonFull(k, t);
+				// if(child[x].leaf==true)
+				// child[x+1].leaf=true;
+			} else {
+				child[x].insertNonFull(k, t);
+			}
+		}
+	}
+
+	void splitChild(int ce, BTreeNode y, int t) {
+		int mid = (y.child[ce].n / 2);
+		int x = n;
+		while (x > ce) {
+			keys[x] = keys[x - 1];
+			x--;
+		}
+		keys[x] = child[ce].keys[mid];
+		int tempChildN = child[ce].n;
+		child[ce].n--;
+		y.n++;
+		BTreeNode nbtn = new BTreeNode(t, true);
+		int i = 0;
+		// int j = mid;
+		while (mid < tempChildN - 1) {
+			nbtn.child[i] = child[ce].child[++mid];
+			nbtn.keys[i++] = child[ce].keys[mid];
+			child[ce].n--;
+		}
+		nbtn.child[i] = child[ce].child[++mid];
+		nbtn.n = i;
+		y.child[ce + 1] = nbtn;
+		if (child[ce].leaf != false) {
+			nbtn.leaf = true;
+		} else {
+			nbtn.leaf = false;
+		}
+	}
+}
+
 public class BTree {
-    
-    private static final int t = 3;
+	int t;
+	BTreeNode root;
 
-    private Node root;       // root of the B-tree
-    private int height;      // height of the B-tree
-    private int n;           // number of key-value pairs in the B-tree
+	BTree(int t) {
+		this.t = t;
+		root = null;
+	}
 
-    // helper B-tree node data type
-    private static final class Node {
-        private int numOfChildren;                             // number of children
-        private Entry[] children = new Entry[2*t - 1];   // the array of children
-
-        // create a node with k children
-        private Node(int k) {
-            numOfChildren = k;
-        }
-    }
-
-    // internal nodes: only use key and next
-    // external nodes: only use key and value
-    private static class Entry {
-        private Integer key;
-        private final Object val;
-        private Node next;     // helper field to iterate over array entries
-        public Entry(Integer key, Object val, Node next) {
-            this.key  = key;
-            this.val  = val;
-            this.next = next;
-        }
-    }
-
-    /**
-     * Initializes an empty B-tree.
-     */
-    public BTree() {
-        root = new Node(0);
-    }
-
-    /**
-     * Returns the value associated with the given key.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *         and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public TrackingDevice get(Integer key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
-        return search(root, key, height);
-    }
-
-    private TrackingDevice search(Node x, Integer key, int ht) {
-        Entry[] children = x.children;
-
-        if (ht == 0) {
-            for (int j = 0; j < x.numOfChildren; j++) {
-                if (eq(key, children[j].key)) return (TrackingDevice) children[j].val;
-            }
-        }
-
-        else {
-            for (int j = 0; j < x.numOfChildren; j++) {
-                if (eq(key, children[j].key)) return (TrackingDevice) children[j].val;
-                
-                if (j+1 == x.numOfChildren || key < children[j+1].key)
-                    return search(children[j].next, key, ht-1);
-            }
-        }
-        return null;
-    }
-
-
-    /**
-     * Inserts the key-value pair into the symbol table, overwriting the old value
-     * with the new value if the key is already in the symbol table.
-     * If the value is {@code null}, this effectively deletes the key from the symbol table.
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void add(TrackingDevice val) {
-        Node u = insert(root, val, height); 
-        n++;
-        if (u == null) return;
-
-        // need to split root
-        Node t = new Node(2);
-        t.children[0] = new Entry(root.children[0].key, null, root);
-        t.children[1] = new Entry(u.children[0].key, null, u);
-        root = t;
-        height++;
-    }
-
-    private Node insert(Node h, TrackingDevice val, int ht) {
-        int j;
-        Entry t = new Entry(val.getId(), val, null);
-
-        // external node
-        if (ht == 0) {
-            for (j = 0; j < h.numOfChildren; j++) {
-                if (less(val.getId(), h.children[j].key)) break;
-            }
-        }
-
-        // internal node
-        else {
-            for (j = 0; j < h.numOfChildren; j++) {
-                if ((j+1 == h.numOfChildren) || less(val.getId(), h.children[j+1].key)) {
-                    Node u = insert(h.children[j++].next, val, ht-1);
-                    if (u == null) return null;
-                    t.key = u.children[0].key;
-                    t.next = u;
-                    break;
+	void add(int k) {
+		// Tree is Empty
+		if (root == null) {
+			root = new BTreeNode(t, true);
+			root.keys[0] = k;
+			root.n++;
+		}
+		// Tree is Not Empty
+		else {
+			// The root Node is full
+			if (root.n == 2 * t - 1) {
+				BTreeNode ch = new BTreeNode(t, false);
+				ch.child[0] = root;
+				root = ch;
+				ch.splitChild(0, root, t);
+				int i = 0;
+				int m = ch.child[0].n - 1;
+				if (ch.child[0].keys[m] < k)
+					i++;
+				ch.child[i].insertNonFull(k, t);
+				// ch.child[1].leaf=true;
+			} else {
+				root.insertNonFull(k, t);
+			}
+		}
+	}
+        
+        Integer get(int k) {
+            BTreeNode n = root.search(k);
+            if(n != null) {
+                for(int x : n.keys) {
+                    if(k == x) return x;
                 }
             }
+            
+            return null;
         }
-
-        for (int i = h.numOfChildren; i > j; i--)
-            h.children[i] = h.children[i-1];
-        h.children[j] = t;
-        h.numOfChildren++;
-        if (less(h.numOfChildren, (2*BTree.t - 1))) return null;
-        else         return split(h);
-    }
-
-    // split node in half
-    private Node split(Node h) {
-        Node t = new Node((2*BTree.t - 1)/2);
-        h.numOfChildren = (2*BTree.t - 1)/2;
-        for (int j = 0; j < (2*BTree.t - 1)/2; j++)
-            t.children[j] = h.children[(2*BTree.t - 1)/2 + j]; 
-        return t;
-    }
-
-    // comparison functions - make Comparable instead of Key to avoid casts
-    private boolean less(Integer k1, Integer k2) {
-        return k1 < k2;
-    }
-
-    private boolean eq(Integer k1, Integer k2) {
-        return k1.equals(k2);
-    }
-    
-    
-
 }
